2cbfbf2fbb4bd63e392ab700163d4436379c0d25
luffyerrot <luffyerrot20@gmail.com>
2024-10-06 00:18:38 +0200

    Create project

---
+++ src/main/java/fr/pierre/medhead/MedheadApplication.java
@@ -0,0 +1,13 @@
+package fr.pierre.medhead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedheadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedheadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/pierre/medhead/MedheadApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.pierre.medhead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedheadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
